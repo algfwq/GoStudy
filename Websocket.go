6dd84a9e7786d697ac30fd68949514fafbfffc80
algfwq <3104374883@qq.com>
2024-05-04 16:35:30 +0800

    数据库和websocket

--- Websocket.go
+++ Websocket.go
@@ -12,17 +12,30 @@ import (
 )
 
 // User 定义一个数据模型(user表)
-// 列名是字段名的蛇形小写(PassWd->pass_word)
 type User struct {
 	Id       uint   `gorm:"AUTO_INCREMENT"`
 	Name     string `gorm:"size:50"`
-	Age      int    `gorm:"size:3"`
+	Age      int
 	Birthday *time.Time
 	Email    string `gorm:"type:varchar(50);unique_index"`
 	PassWord string `gorm:"type:varchar(25)"`
 }
 
+// BeforeCreate GORM hook
+func (u *User) BeforeCreate(scope *gorm.Scope) error {
+	// 实现Birthday自动设置
+	if u.Birthday == nil {
+		currentTime := time.Now()
+		if err := scope.SetColumn("Birthday", &currentTime); err != nil {
+			log.Println("birthday设置出错：", err)
+		}
+	}
+	return nil
+}
+
 var db *gorm.DB
+var logFile *os.File
+var err error
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  8192,
@@ -32,103 +45,141 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func main() {
-	//处理日志保存
-	// 创建日志文件
-	logFile, err := os.OpenFile("example.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+func init() {
+	//数据库操作
+	//db, err := gorm.Open("mysql", "root:123456@(127.0.0.1:3306)/qiu?charset=utf8&parseTime=True&loc=Local")
+	// 初始化数据库连接
+	username := "root"
+	password := "123456"
+	host := "127.0.0.1"
+	port := 3306
+	dbname := "qiu"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, port, dbname)
+	db, err = gorm.Open("mysql", dsn)
 	if err != nil {
-		log.Fatal(err) // 如果无法创建文件，使用 Fatal 日志并终止程序
+		log.Fatalf("创建数据库连接失败: %v", err)
 	}
-	defer logFile.Close() // 确保文件在函数结束时关闭
+
+	// 自动迁移数据结构
+	db.AutoMigrate(&User{})
+
+	// 添加唯一索引
+	//db.Model(&User{}).AddUniqueIndex("name_email", "name", "age")
+
+	// 初始化日志文件
+	logFile, err = os.OpenFile("example.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalf("打开文件失败: %v", err)
+	}
 
 	// 设置日志输出到文件
 	log.SetOutput(logFile)
 
-	// 记录日志
-	log.Println("这是一个日志记录")
-	log.Println("这是另一个日志记录")
+	// 记录日志文件初始化成功的消息
+	log.Println("日志文件初始化成功")
+}
+
+func main() {
+	defer mainExit()
 
 	// 创建HTTP服务器
 	http.HandleFunc("/ws", handleWebSocket)
-	log.Println("Server started on :8080")
+	log.Println("服务器开启在端口:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	// 升级HTTP连接为WebSocket连接
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
+	conn, errGrader := upgrader.Upgrade(w, r, nil)
+	if errGrader != nil {
+		log.Println("连接升级失败：", errGrader)
 		return
 	}
-	defer conn.Close()
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Printf("关闭连接失败: %v", err)
+		}
+	}()
 
-	// 处理WebSocket连接
 	for {
-		// 读取消息
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
+		messageType, p, errRead := conn.ReadMessage()
+		if errRead != nil {
+			log.Println("读取消息失败/连接关闭", errRead)
 			return
 		}
-		log.Println("Received message:", string(p))
-		if string(p) == "rom" {
-			//数据库操作
-			db, err := gorm.Open("mysql", "root:123456@(127.0.0.1:3306)/qiu?charset=utf8&parseTime=True&loc=Local")
-			if err != nil {
-				log.Fatalf("创建数据库连接失败:%v", err)
-			}
-			//defer db.Close()
+		log.Println("收到消息:", string(p))
 
-			// 自动迁移数据结构(table schema)
-			// 注意:在gorm中，默认的表名都是结构体名称的复数形式，比如User结构体默认创建的表为users
-			// db.SingularTable(true) 可以取消表名的复数形式，使得表名和结构体名称一致
-			db.AutoMigrate(&User{})
+		// 示例：根据接收到的消息插入新用户
+		//newUser := User{Name: "bgbiao", Age: 99999, Email: "bgbia"}
+		//result := db.Create(&newUser)
+		//if errCreate := result.Error; errCreate != nil {
+		//	log.Println("创建数据失败：", errCreate)
+		//} else {
+		//	log.Printf("创建数据成功, %d rows affected", result.RowsAffected)
+		//}
 
-			// 添加唯一索引
-			db.Model(&User{}).AddUniqueIndex("name_email", "id", "name", "email")
+		// start
+		// 插入记录
+		db.Create(&User{Name: "bgbiao", Age: 99999, Email: "bgbiao@bgbiao.top"})
+		db.Create(&User{Name: "xxb", Age: 18, Email: "xxb@bgbiao.top"})
+		db.Create(&User{Name: "xxb", Age: 18, Email: "xxb@bgbiao.to"})
 
-			// 插入记录
-			db.Create(&User{Name: "bgbiao", Age: 18, Email: "bgbiao@bgbiao.top"})
-			db.Create(&User{Name: "xxb", Age: 18, Email: "xxb@bgbiao.top"})
-			db.Create(&User{Name: "于泽", Age: 14, Email: "3104374883@qq.com", PassWord: "xxxx"})
+		var user User
+		var user2 User
+		var users []User
+		var users1 []User
+		var users2 []User
+		var users3 []User
+		// 查看插入后的全部元素
+		log.Printf("插入后元素:\n")
+		db.Find(&users)
+		log.Println(users)
 
-			var user User
-			var users []User
-			// 查看插入后的全部元素
-			fmt.Printf("插入后元素:\n")
-			log.Printf("插入后元素:\n")
-			db.Find(&users)
-			fmt.Println(users)
-			log.Println(users)
+		// 查询一条记录
+		db.First(&user, "name = ?", "bgbiao")
+		log.Println("查看查询记录:", user)
 
-			// 查询一条记录
-			db.First(&user, "name = ?", "bgbiao")
-			fmt.Println("查看查询记录:", user)
-			log.Println("查看查询记录:", user)
+		db.First(&user2, "age = ?", 18)
+		log.Println("查看记录2：", user2)
 
-			// 更新记录(基于查出来的数据进行更新)
-			db.Model(&user).Update("name", "biaoge")
-			fmt.Println("更新后的记录:", user)
-			log.Println("更新后的记录:", user)
+		// 更新记录(基于查出来的数据进行更新)
+		db.Model(&user2).Update("name", "biaoge")
+		log.Println("更新后的记录:", user2)
 
-			// 删除记录
-			db.Delete(&user)
+		//复杂查询
+		db.Where("name = ?", "xxb").Where("age = ?", 18).Find(&users1)
+		db.Where("name = ?", "biaoge").Or("age = ?", 18).Find(&users2)
+		db.Where("name = ?", "John").Or(db.Where("name = ?", "biaoge").Where("age = ?", 99999)).Find(&users3)
+		log.Println("users1：", users1)
+		log.Println("users2：", users2)
+		log.Println("users3：", users3)
 
-			// 查看全部记录
-			fmt.Println("查看全部记录:")
-			log.Println("查看全部记录:")
+		// 删除记录
+		db.Delete(&user)
 
-			db.Find(&users)
-			fmt.Println(users)
-			log.Println(users)
-		}
+		// 查看全部记录
+		log.Println("查看全部记录:")
 
-		// 发送消息
-		err = conn.WriteMessage(messageType, []byte("Hello, world!"))
-		if err != nil {
-			log.Println(err)
+		db.Find(&users)
+		log.Println(users)
+		// end
+
+		errSend := conn.WriteMessage(messageType, []byte("Hello, world!"))
+		if errSend != nil {
+			log.Println("发送数据失败：", errSend)
 			return
 		}
+	}
+}
+
+func mainExit() {
+	// 程序结束前关闭数据库连接
+	if err := db.Close(); err != nil {
+		log.Printf("关闭数据库连接失败: %v", err)
+	}
+	// 确保日志文件在程序结束时关闭
+	// 注意：因为 init() 函数没有直接的方法来在程序结束时执行代码，
+	// 通常需要在main()中或程序的其他适当位置显式调用关闭文件的操作。
+	if err := logFile.Close(); err != nil {
+		log.Printf("关闭logFile失败: %v", err)
 	}
 }
